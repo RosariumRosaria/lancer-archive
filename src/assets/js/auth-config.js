d2cb0262c50ae891d49f497d02531ca4718d8f5c
Eve <eviedischinger@gmail.com>
2026-01-29 15:51:17 -0500

    CHAR

--- src/assets/js/auth-config.js
+++ src/assets/js/auth-config.js
@@ -1,6 +1,6 @@
 window.ARCHIVE_ACCESS = {
     "KEVTOWN": { user: "KEVIN", clearance: 2 },
     "ISEEYOU": { user: "OVERSIGHT", clearance: 999 },
-    "COWBOYHIMBO": { user: "Charley", clearance: 1 },
+    "COWBOYHIMBO": { user: "CHARLEY", clearance: 1 },
     "GUEST": { user: "GUEST", clearance: 1 }
 };
